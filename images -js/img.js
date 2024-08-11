ee4bdfa1953a57d738318ff73ff6e643cabd5f93
sushma <ksushmaacharya@gmail.com>
2024-08-11 09:50:13 +0530

    final code added

--- images -js/img.js
+++ images -js/img.js
@@ -3,7 +3,7 @@ let s2=document.querySelector(".para2-2");
 let s3=document.querySelector(".para2-3");
 let video=document.querySelector(".video");
 
-s1.addEventListener("mousemove",()=>{
+s1.addEventListener("mouseenter",()=>{
     video.src="/images -js/earthquakevideo.mp4";
 });
 
@@ -12,6 +12,6 @@ s2.addEventListener("mouseenter",()=>{
     video.load();
 });
 
-s3.addEventListener("mousemove",()=>{
+s3.addEventListener("mouseenter",()=>{
     video.src="/images -js/droughtvideo.mp4";
 });
